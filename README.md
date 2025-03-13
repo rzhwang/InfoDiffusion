275bf79ba310062f03d99a96a3d19eee9315166d
renzhiwang <renzhiwang@tencent.com>
2025-03-13 17:43:17 +0800

    new file:   basic_utils.py 	new file:   datasets/CommonsenseConversation/train.jsonl 	new file:   datasets/CommonsenseConversation/valid.jsonl 	new file:   img/infodiffusion.png 	new file:   img/landscape_poster_InfoDiffusion.pdf 	new file:   infodiffusion/__init__.py 	new file:   infodiffusion/config.json 	new file:   infodiffusion/gaussian_diffusion.py 	new file:   infodiffusion/rounding.py 	new file:   infodiffusion/step_sample.py 	new file:   infodiffusion/text_datasets.py 	new file:   infodiffusion/transformer_model.py 	new file:   infodiffusion/utils/__init__.py 	new file:   infodiffusion/utils/dist_util.py 	new file:   infodiffusion/utils/fp16_util.py 	new file:   infodiffusion/utils/logger.py 	new file:   infodiffusion/utils/losses.py 	new file:   infodiffusion/utils/nn.py 	new file:   requirements.txt 	new file:   sample_seq2seq.py 	new file:   scripts/eval_seq2seq.py 	new file:   scripts/run_decode.py 	new file:   scripts/run_decode.sh 	new file:   scripts/run_train.py 	new file:   scripts/train.sh 	new file:   train.py 	new file:   train_util.py

--- README.md
+++ README.md
@@ -1,2 +1,100 @@
 # InfoDiffusion
-Code for "InfoDiffusion: Information Entropy Aware Diffusion Process for Non-Autoregressive Text Generation"
+
+Official Codebase for "InfoDiffusion: Information Entropy Aware Diffusion Process for Non-Autoregressive Text Generation"
+
+<p align = "center">
+<img src="img/infodiffusion.png" width="95%" alt="" align=center />
+</p>
+<p align = "center">
+The diffusion process of InfoDiffusion.
+</p>
+
+## Setup:
+
+The code is based on PyTorch and HuggingFace `transformers`.
+
+```bash
+pip install -r requirements.txt 
+```
+
+## Datasets
+
+Prepare datasets and put them under the `datasets` folder. Take `datasets/CommonsenseConversation/train.jsonl` as an example. We use four datasets in our paper.
+
+| Task                 | Datasets                 | Training Samples | Source                                               | Used in InfoDiffusion                                                                         |
+| -------------------- | ------------------------ | ---------------- | ---------------------------------------------------- | --------------------------------------------------------------------------------------------- |
+| Open-domain Dialogue | Commonsense Conversation | 3382k            | [CCM](https://github.com/thu-coai/ccm)                  | [download](https://drive.google.com/drive/folders/1exENF9Qc5UtXnHlNl9fvaxP3zyyH32qp?usp=sharing) |
+| Question Generation  | Quasar-T                 | 117k             | [OpenQA](https://github.com/thunlp/OpenQA)              | [download](https://drive.google.com/drive/folders/122YK0IElSnGZbPMigXrduTVL1geB4wEW?usp=sharing) |
+| Text Simplification  | Wiki-alignment           | 677k             | [Wiki-auto](https://github.com/chaojiang06/wiki-auto)   | [download](https://drive.google.com/drive/folders/1BlWtD1UbnL_ef06Riq-gABlL0Zb50s-d?usp=sharing) |
+| Paraphrase           | QQP                      | 144k             | [Kaggle](https://www.kaggle.com/c/quora-question-pairs) | [download](https://drive.google.com/drive/folders/1BHGCeHRZU7MQF3rsqXBIOCU2WIC3W6fb?usp=sharing) |
+
+## InfoDiffusion Training
+
+```bash
+cd scripts
+bash train.sh
+```
+
+Arguments explanation:
+
+- ``--dataset``: the name of datasets, just for notation
+- ``--data_dir``: the path to the saved datasets folder, containing ``train.jsonl,test.jsonl,valid.jsonl``
+- ``--seq_len``: the max length of sequence $z$ ($x\oplus y$)
+- ``--resume_checkpoint``: if not none, restore this checkpoint and continue training
+- ``--vocab``: the tokenizer is initialized using bert or load your own preprocessed vocab dictionary (e.g. using BPE)
+
+### Update:
+
+Additional argument:
+
+- ``--learned_mean_embed``: set whether to use the learned soft absorbing state.
+- ``--denoise``: set whether to add discrete noise
+- ``--use_fp16``: set whether to use mixed precision training
+- ``--denoise_rate``: set the denoise rate, with 0.5 as the default
+
+```
+python -m torch.distributed.launch --nproc_per_node=4 --master_port=12233 --use_env run_train.py --diff_steps 2000 --lr 0.0001 --learning_steps 50000 --save_interval 10000 --seed 102 --noise_schedule info --hidden_dim 128 --bsz 2048 --dataset qqp --data_dir {datasets/QQP} --vocab bert --seq_len 128 --schedule_sampler lossaware --notes qqp
+```
+
+Empirically, larger batchsize (larger `microbatch` here) can achieve higher BLEU score (without MBR). If you want to sync training loss to wandb, please customize your wandb setting in `train.py` (add your own API KEY).
+
+## InfoDiffusion Decoding
+
+You need to modify the path to ``model_dir``, which is obtained in the training stage.
+
+```bash
+cd scripts
+bash run_decode.sh
+```
+
+To reproduce the results of Table 1 in our paper, we suggest the size of MBR candidate set to be 10 (run 10 times using different seeds). Empirically, larger size can achieve higher BLEU score. For diversity metrics, the size of MBR candidate set is 3 when computing.
+
+## Evaluation & MBR
+
+You need to specify the folder of decoded texts. This folder should contain the decoded files from the same model but sampling with different random seeds. If ``mbr`` is not attached, we will compute the diversity score from the files in the folder, otherwise we will do MBR decoding:
+
+```bash
+cd scripts
+python eval_seq2seq.py --folder ../{your-path-to-outputs} --mbr
+```
+
+Note: if you want to use this evaluation script for output files from other models, please make sure the same line from these output files refers to the same piece of data. Otherwise the diversity score could be incorrect.
+
+## Acknowledgement
+
+This repo benefits from [DiffuSeq](https://github.com/Shark-NLP/DiffuSeq/) and [Diffusion-LM](https://github.com/XiangLi1999/Diffusion-LM). Thanks for their wonderful works.
+
+## Citation
+
+Please add the citation if our paper or code helps you.
+
+```
+@inproceedings{wang-etal-2023-infodiffusion,
+    title = "{I}nfo{D}iffusion: Information Entropy Aware Diffusion Process for Non-Autoregressive Text Generation",
+    author = "Wang, Renzhi  and
+      Li, Jing  and
+      Li, Piji",
+    booktitle = "Findings of the Association for Computational Linguistics: EMNLP 2023",
+    year = "2023",
+}
+```
---
+++ sample_seq2seq.py
@@ -0,0 +1,224 @@
+"""
+Generate a large batch of image samples from a model and save them as a large
+numpy array. This can be used to produce samples for FID evaluation.
+"""
+
+import argparse
+import os, json
+from tracemalloc import start
+
+import numpy as np
+import torch as th
+import torch.distributed as dist
+from transformers import set_seed
+from infodiffusion.rounding import denoised_fn_round
+from infodiffusion.text_datasets import load_data_text
+
+# from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
+
+import time
+from infodiffusion.utils import dist_util, logger
+from functools import partial
+from basic_utils import (
+    load_defaults_config,
+    create_model_and_diffusion,
+    add_dict_to_argparser,
+    args_to_dict,
+    load_tokenizer
+)
+
+def create_argparser():
+    defaults = dict(model_path='', step=0, out_dir='', top_p=0)
+    decode_defaults = dict(split='valid', clamp_step=0, seed2=105, clip_denoised=False)
+    defaults.update(load_defaults_config())
+    defaults.update(decode_defaults)
+    parser = argparse.ArgumentParser()
+    add_dict_to_argparser(parser, defaults)
+    return parser
+
+
+@th.no_grad()
+def main():
+    args = create_argparser().parse_args()
+
+    dist_util.setup_dist()
+    logger.configure()
+
+    world_size = dist.get_world_size() or 1
+    rank = dist.get_rank() or 0
+
+    # load configurations.
+    config_path = os.path.join(os.path.split(args.model_path)[0], "training_args.json")
+    print(config_path)
+    # sys.setdefaultencoding('utf-8')
+    with open(config_path, 'rb', ) as f:
+        training_args = json.load(f)
+    training_args['batch_size'] = args.batch_size
+    args.__dict__.update(training_args)
+
+    logger.log("### Creating model and diffusion...")
+    model, diffusion = create_model_and_diffusion(
+        **args_to_dict(args, load_defaults_config().keys())
+    )
+
+    model.load_state_dict(
+        dist_util.load_state_dict(args.model_path, map_location="cpu")
+    )
+
+    pytorch_total_params = sum(p.numel() for p in model.parameters())
+    logger.log(f'### The parameter count is {pytorch_total_params}')
+
+    model.eval().requires_grad_(False).to(dist_util.dev())
+
+    tokenizer = load_tokenizer(args)
+    model_emb = th.nn.Embedding(
+        num_embeddings=tokenizer.vocab_size, 
+        embedding_dim=args.hidden_dim, 
+        _weight=model.word_embedding.weight.clone().cpu()
+    ).eval().requires_grad_(False)
+
+    set_seed(args.seed2)
+
+    print("### Sampling...on", args.split)
+
+    ## load data
+    data_valid = load_data_text(
+        batch_size=args.batch_size,
+        seq_len=args.seq_len,
+        deterministic=True,
+        data_args=args,
+        split=args.split,
+        loaded_vocab=tokenizer,
+        model_emb=model_emb.cpu(),  # using the same embedding wight with tranining data
+        loop=False
+    )
+
+    start_t = time.time()
+
+    # batch, cond = next(data_valid)
+    # print(batch.shape)
+
+    model_base_name = os.path.basename(os.path.split(args.model_path)[0]) + f'.{os.path.split(args.model_path)[1]}'
+    out_dir = os.path.join(args.out_dir, f"{model_base_name.split('.ema')[0]}")
+    if not os.path.isdir(out_dir):
+        os.mkdir(out_dir)
+
+    out_path = os.path.join(out_dir, f"ema{model_base_name.split('.ema')[1]}.samples")
+    if not os.path.isdir(out_path):
+        os.mkdir(out_path)
+    out_path = os.path.join(out_path, f"seed{args.seed2}_step{args.clamp_step}.json")
+    # fout = open(out_path, 'a')
+
+    all_test_data = []
+
+    idx = 0
+
+    try:
+        while True:
+            batch, cond = next(data_valid)
+            # print(batch.shape)
+            if idx % world_size == rank:  # Split data per nodes
+                all_test_data.append(cond)
+            idx += 1
+
+    except StopIteration:
+        print('### End of reading iteration...')
+
+    model_emb.to(dist_util.dev())
+
+    if idx % world_size and rank >= idx % world_size:
+        all_test_data.append({})  # Dummy data for Remainder : for dist.barrier()
+
+    if rank == 0:
+        from tqdm import tqdm
+        iterator = tqdm(all_test_data)
+    else:
+        iterator = iter(all_test_data)
+
+    for cond in iterator:
+
+        if not cond:  # Barrier for Remainder
+            for i in range(world_size):
+                dist.barrier()
+            continue
+
+        input_ids_x = cond.pop('input_ids').to(dist_util.dev())
+        x_start = model.get_embeds(input_ids_x)
+        input_ids_mask = cond.pop('input_mask')
+        input_ids_mask_ori = input_ids_mask
+
+        noise = th.randn_like(x_start)
+        input_ids_mask = th.broadcast_to(input_ids_mask.unsqueeze(dim=-1), x_start.shape).to(dist_util.dev())
+        x_noised = th.where(input_ids_mask == 0, x_start, noise)
+
+        model_kwargs = {}
+
+        if args.step == args.diffusion_steps:
+            args.use_ddim = False
+            step_gap = 1
+        else:
+            args.use_ddim = True
+            step_gap = args.diffusion_steps//args.step
+
+        sample_fn = (
+            diffusion.p_sample_loop if not args.use_ddim else diffusion.ddim_sample_loop
+        )
+
+        sample_shape = (x_start.shape[0], args.seq_len, args.hidden_dim)
+
+        samples = sample_fn(
+            model,
+            sample_shape,
+            noise=x_noised,
+            clip_denoised=args.clip_denoised,
+            denoised_fn=partial(denoised_fn_round, args, model_emb),
+            model_kwargs=model_kwargs,
+            top_p=args.top_p,
+            clamp_step=args.clamp_step,
+            clamp_first=True,
+            mask=input_ids_mask,
+            x_start=x_start,
+            gap=step_gap
+        )
+
+        # print(samples[0].shape) # samples for each step
+
+        sample = samples[-1]
+
+        # print('decoding for seq2seq', )
+        # print(sample.shape)
+
+        logits = model.get_logits(sample)  # bsz, seqlen, vocab
+        cands = th.topk(logits, k=1, dim=-1)
+
+        word_lst_recover = []
+        word_lst_ref = []
+        word_lst_source = []
+
+        # tokenizer = load_tokenizer(args)
+
+        for seq, input_mask in zip(cands.indices, input_ids_mask_ori):
+            len_x = args.seq_len - sum(input_mask).tolist()
+            tokens = tokenizer.decode_token(seq[len_x:])
+            word_lst_recover.append(tokens)
+
+        for seq, input_mask in zip(input_ids_x, input_ids_mask_ori):
+            # tokens = tokenizer.decode_token(seq)
+            len_x = args.seq_len - sum(input_mask).tolist()
+            word_lst_source.append(tokenizer.decode_token(seq[:len_x]))
+            word_lst_ref.append(tokenizer.decode_token(seq[len_x:]))
+
+        for i in range(world_size):
+            if i == rank:  # Write files sequentially
+                fout = open(out_path, 'a')
+                for (recov, ref, src) in zip(word_lst_recover, word_lst_ref, word_lst_source):
+                    print(json.dumps({"recover": recov, "reference": ref, "source": src}), file=fout)
+                fout.close()
+            dist.barrier()
+
+    print('### Total takes {:.2f}s .....'.format(time.time() - start_t))
+    print(f'### Written the decoded output to {out_path}')
+
+
+if __name__ == "__main__":
+    main()
